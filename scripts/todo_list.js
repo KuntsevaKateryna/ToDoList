a7b6ec0a6fd5f450ee48e5eed876d3fc3834f52f
kaku0715 <kateryna.kuntseva@NetCracker.com>
2025-02-15 11:20:31 +0200

    replace 'for loop' cycle with forEach function

--- scripts/todo_list.js
+++ scripts/todo_list.js
@@ -1,11 +1,4 @@
-const todoArr = [
-    {name: 'first to do morning exercises',
-     duedate: '2025-01-09'   
-    },
-    {name: 'english',
-    duedate: '2025-01-09'}
-];
-
+const todoArr = [];
 
 function addTodo () {
     const todoVal = document.querySelector('.js-input-todo');
@@ -20,38 +13,27 @@ function addTodo () {
    
  console.log(todoArr);
  exposeTodoList() ;
-
 }
 
 function exposeTodoList() {
     let div_todo_HTML = '';
-    
-    for (let i=0; i<todoArr.length; i++) {
-        const todo_Object = todoArr[i];
+    todoArr.forEach( function(value, index) {
+        const todo_Object = todoArr[index];
         const name = todo_Object.name;
         const dueDate = todo_Object.duedate;
         const html = 
             `<div> ${name}</div>
              <div>${dueDate} </div>
             <button  class="js-button-delete"
-                onclick="todoArr.splice(${i}, 1);
+                onclick="todoArr.splice(${index}, 1);
                 exposeTodoList();">
             Delete
             </button>`
- /*           `<p> ${name}
-            ${dueDate} 
-            <button 
-                onclick="todoArr.splice(${i}, 1);
-                exposeTodoList();">
-            Delete
-            </button>
-            </p>`
-            */
         div_todo_HTML += html;
-       // div_todo_HTML += `<p> ${todo_item} <\p>`
-    }
+        }
+
+    );
     console.log(div_todo_HTML);
     document.querySelector('.js-div_todo_list').innerHTML = div_todo_HTML;
-  
     document.querySelector('.js-input-todo').value = '';
 }
